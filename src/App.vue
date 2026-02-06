fb8e95c695b068eff91b276af857116eef848b36
akimanagabriel <akimanagabriel@gmail.com>
2026-02-06 09:57:24 +0200

    add initial files

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Game/>
+  </div>
+</template>
+<script setup>
+import Game from './Game.vue';
+
+</script>
---
+++ src/Game.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>TICTAC GAME</h1>
+
+  <!-- tictac board -->
+  <div class="container">
+    <div class="square" v-for="cell in 9"></div>
+  </div>
+
+  <ol>
+    <li v-for="name in names">{{ name }}</li>
+  </ol>
+</template>
+
+<style scoped>
+.square {
+  border: 2px grey solid;
+}
+.square:nth-child(even) {
+    background-color: teal;
+}
+
+.container {
+  height: 200px;
+  width: 200px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2px;
+  border: 5px solid orangered;
+  padding: 2px;
+}
+</style>
+
+<script setup>
+const names = [
+  "Japhet",
+  "Johan",
+  "Joanna",
+  "Jean",
+  "Jules",
+  "Junior",
+  "Caella",
+  "Hertier",
+];
+</script>
