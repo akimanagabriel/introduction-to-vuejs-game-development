613071fcaf1137209d16d00b4195e92ec8e5dfac
akimanagabriel <akimanagabriel@gmail.com>
2026-02-09 08:56:20 +0200

    add live search

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Hooks />
     <button @click="isHidden = !isHidden">show/hide</button>
     <Game v-if="isHidden" />
   </div>
@@ -7,6 +8,7 @@
 <script setup>
 import { ref } from "vue";
 import Game from "./Game.vue";
+import Hooks from "./Hooks.vue";
 
 const isHidden = ref(false);
 </script>
---
+++ src/Hooks.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onUpdated, ref } from "vue";
+
+const name = ref("");
+
+const students = ref(["Roger", "Caella", "Kylo", "Muhire", "Aime"]);
+
+onUpdated(function () {
+  //   console.log("The state is changedn to ", name.value);
+  if (name.value.length !== 0) {
+    students.value = students.value.filter((student) =>
+      student.toLowerCase().includes(name.value.toLowerCase())
+    );
+  } else {
+    students.value = ["Roger", "Caella", "Kylo", "Muhire", "Aime"];
+  }
+});
+// onUpdated(()=>{})
+</script>
+
+<template>
+  <input type="text" v-model="name" />
+  <h1>Hello {{ name }}</h1>
+  <hr />
+  <ul>
+    <li v-for="std in students">{{ std }}</li>
+  </ul>
+</template>
