20e07169e5ff1765289bdff91f02b474981cb170
akimanagabriel <akimanagabriel@gmail.com>
2026-03-03 17:24:51 +0200

    add api files

---
+++ README.md
@@ -0,0 +1,12 @@
+Task Overview
+
+You must fetch data from:
+
+https://fakestoreapi.com/products
+
+And display all products in a layout with 3 columns grid.
+
+Submission Deadline: 03 / March / 2026 before 11:59
+
+How to Submit:
+Deploy your app on Netlify or Vercel and share the link in the designated area on Google Classroom.
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="item in products">
+    <img height="200" :src="item.image" alt="" />
+    <h1>{{ item.title }}</h1>
+    <p>{{ item.description }}</p>
+    <hr />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import api from "./axios_config";
+
+const products = ref([]);
+
+onMounted(() => {
+  api.get("/products").then((res) => {
+    products.value = res.data;
+  });
+});
+</script>
