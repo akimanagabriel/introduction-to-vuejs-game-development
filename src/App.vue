09ee415e24cd4cf8a43bbbdfe799de761fa22c39
akimanagabriel <akimanagabriel@gmail.com>
2026-03-06 10:42:05 +0200

    call recipe api

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <template>
-  <div v-for="item in products">
-    <img height="200" :src="item.image" alt="" />
-    <h1>{{ item.title }}</h1>
-    <p>{{ item.description }}</p>
-    <hr />
+  <div class="m-10">
+    <div class="grid grid-cols-3 gap-5">
+      <!-- single recipe -->
+      <div class="shadow-xl p-5 rounded-xl" v-for="recipe in recipes">
+        <img class="rounded-xl" loading="lazy" :src="recipe.image" alt="" />
+        <h1 class="line-clamp-1 text-orange-500 font-bold my-3 text-3xl">{{ recipe.name }}</h1>
+
+        <!-- tags -->
+        <div class="grid grid-cols-3 gap-3">
+          <div class="px-4 py-1 bg-orange-100 text-orange-800 rounded-full line-clamp-1 text-center" v-for="tag in recipe.tags">{{ tag }}</div>
+        </div>
+
+        <h3 class="font-bold underline mb-4">Instructions</h3>
+        <ol>
+          <li class="my-1 line-clamp-1" v-for="(rule, i) in recipe.instructions">
+            {{ i + 1 }}. {{ rule }}
+          </li>
+        </ol>
+
+        <h3 class="font-bold underline mb-4">Ingredients</h3>
+        <ol>
+          <li class="my-1 line-clamp-1" v-for="(rule, i) in recipe.ingredients">
+            {{ i + 1 }}. {{ rule }}
+          </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import axios from "axios";
 import { onMounted, ref } from "vue";
-import api from "./axios_config";
 
-const products = ref([]);
+const recipes = ref([]);
 
 onMounted(() => {
-  api.get("/products").then((res) => {
-    products.value = res.data;
+  // calling api
+  axios.get("https://dummyjson.com/recipes").then((response) => {
+    recipes.value = response.data.recipes;
+    console.log(recipes.value);
   });
 });
 </script>
